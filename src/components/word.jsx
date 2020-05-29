b1018eb4847e6a54d678b8d314a4d97bfe0e4815
Joshua Book <joshua.book@gmail.com>
2020-05-29 16:29:31 -0400

    Add translation through GoogleAPI from from to to.

--- src/components/word.jsx
+++ src/components/word.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import wretch from 'wretch';
 
 const WordBox = styled.div`
   display: flex;
@@ -22,13 +23,37 @@ const WordBox = styled.div`
 
 const Word = ({word}) => {
   const [from, setFrom] = useState(word.from)
+  const [to, setTo] = useState(word.to)
+
+  const translate = (from) => {
+    setFrom(from)
+
+    const data = {
+      "source": "es",
+      "q": from,
+      "target": "en"
+    }
+
+    wretch()
+      .url("https://translation.googleapis.com/language/translate/v2")
+      .auth("Bearer ya29.c.Ko8BzQevjMNBB7SqOpKe82I0LMBbRVNjiodbdW4h1BMNWoMiMy-p_Pur9w8QZ48xTAcEomFnhWlD1HhUKWLTHFtSL3NF9h3Vqs1wAbf97dVS9EVUWyW50UNe0vaIKk75hp5LhO8hKZYrQ50KgVNf_163kC-TT7W9Gexb5zGGRkop5ui5EsT7g8x3WWsZlGGWc0o")
+      .post(data)
+      .json(response => {
+        console.log('response', response);
+        setTo(response.data.translations[0].translatedText)
+      })
+      .catch(err => {
+        console.log('error', err);
+      });
+  }
+
   return (
     <WordBox>
       <div>
-        <input value={from} onChange={e => setFrom(e.target.value) }/>
+        <input value={from} onChange={e => translate(e.target.value) }/>
       </div>
       <div>
-        {word.to}
+        {to}
       </div>
     </WordBox>
   )
