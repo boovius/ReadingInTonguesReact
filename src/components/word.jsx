b35fe2d027b392b84b4e2b094602dc47e8f617d9
Joshua Book <joshua.book@gmail.com>
2020-06-02 14:20:02 -0400

    Fix debouncing and move translation request to local server.

--- src/components/word.jsx
+++ src/components/word.jsx
@@ -26,36 +26,37 @@ const Word = ({word}) => {
   const [from, setFrom] = useState(word.from)
   const [to, setTo] = useState(word.to)
 
-  const translate = (from) => {
+  const handleInputChange = (from) => {
+    console.log('handling input change on', from)
     setFrom(from)
-
-    debounce(() => {
-      console.log('debouncing?')
-
-      const data = {
-        "source": "es",
-        "q": from,
-        "target": "en"
-      }
-
-      wretch()
-        .url("https://translation.googleapis.com/language/translate/v2")
-        .auth("Bearer ya29.c.Ko8BzQevjMNBB7SqOpKe82I0LMBbRVNjiodbdW4h1BMNWoMiMy-p_Pur9w8QZ48xTAcEomFnhWlD1HhUKWLTHFtSL3NF9h3Vqs1wAbf97dVS9EVUWyW50UNe0vaIKk75hp5LhO8hKZYrQ50KgVNf_163kC-TT7W9Gexb5zGGRkop5ui5EsT7g8x3WWsZlGGWc0o")
-        .post(data)
-        .json(response => {
-          console.log('response', response);
-          setTo(response.data.translations[0].translatedText)
-        })
-        .catch(err => {
-          console.log('error', err);
-        });
-    }, 200)
+    translate(from)
   }
+
+  const translate = debounce((from) => {
+    console.log('debouncing?')
+    console.log('translating: ', from)
+
+    const data = {
+      "from": from,
+      "target": "en"
+    }
+
+    wretch()
+      .url("http://localhost:8080")
+      .post(data)
+      .json(response => {
+        console.log('response', response);
+        setTo(response.translations.join(', '));
+      })
+      .catch(err => {
+        console.log('error', err);
+      });
+  }, 1000)
 
   return (
     <WordBox>
       <div>
-        <input value={from} onChange={e => translate(e.target.value) }/>
+        <input value={from} onChange={e => handleInputChange(e.target.value) }/>
       </div>
       <div>
         {to}
