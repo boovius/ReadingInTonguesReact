6f9dbfadb61a92c2bd5e29f4cebc49a9d99c64fc
Joshua Book <joshua.book@gmail.com>
2020-06-03 16:54:47 -0400

    Fix select component to show proper value after change.

--- src/components/select-tongue.jsx
+++ src/components/select-tongue.jsx
@@ -1,26 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const SelectTongue = ({tongueCodes, setTarget, initialTarget}) => (
-  <select
-    onChange={e => setTarget(e.target.value) }
-    name='tongue'
-    id='tongue'
-    value='en'
-  >
-    {
-      tongueCodes.sort((a,b) => {
-        if (a.tongue < b.tongue) {
-          return -1
-        } else if (a.tongue > b.tongue) {
-          return 1
-        } else {
-          return 0
-        }
-      }).map(({tongue, code}, i) => (
-        <option key={i} value={code}>{tongue}</option>
-      ))
-    }
-  </select>
-)
+const SelectTongue = ({tongueCodes, setTarget, initialTarget}) => {
+  const [val, setVal] = useState(initialTarget);
+  const updateVal = (val) => {
+    setVal(val);
+    setTarget(val);
+  }
+
+  return (
+    <select
+      onChange={e => updateVal(e.target.value) }
+      name='tongue'
+      id='tongue'
+      value={val}
+    >
+      {
+        tongueCodes.sort((a,b) => {
+          if (a.tongue < b.tongue) {
+            return -1
+          } else if (a.tongue > b.tongue) {
+            return 1
+          } else {
+            return 0
+          }
+        }).map(({tongue, code}, i) => (
+          <option key={i} value={code}>{tongue}</option>
+        ))
+      }
+    </select>
+  )
+}
 
 export default SelectTongue;
